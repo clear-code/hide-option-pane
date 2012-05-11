e8f86126ab8c9de756defd353cbbde54ca20ee9e
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-05-11 12:09:53 +0900

    Add new default preferences for recent versions of Thunderbird

--- defaults/preferences/preferences-customizer.js
+++ defaults/preferences/preferences-customizer.js
@@ -9,6 +9,8 @@ pref("extensions.preferencescustomizer.browser.paneAdvanced.enabled", true);
 pref("extensions.preferencescustomizer.messenger.paneGeneral.enabled", true);
 pref("extensions.preferencescustomizer.messenger.paneDisplay.enabled", true);
 pref("extensions.preferencescustomizer.messenger.paneCompose.enabled", true);
-pref("extensions.preferencescustomizer.messenger.panePrivacy.enabled", true);
-pref("extensions.preferencescustomizer.messenger.paneDownloads.enabled", true);
+pref("extensions.preferencescustomizer.messenger.paneSecurity.enabled", true);
+pref("extensions.preferencescustomizer.messenger.paneApplications.enabled", true);
+pref("extensions.preferencescustomizer.messenger.panePrivacy.enabled", true); // for old Thunderbird
+pref("extensions.preferencescustomizer.messenger.paneDownloads.enabled", true); // for old Thunderbird
 pref("extensions.preferencescustomizer.messenger.paneAdvanced.enabled", true);
