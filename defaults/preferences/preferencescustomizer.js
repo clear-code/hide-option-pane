d397ea359e8f8cd4a97765724c40b9b8f5127c51
YUKI Hiroshi <shimoda@clear-code.com>
2013-12-24 18:16:18 +0900

    Add default preference for the sync pane

--- defaults/preferences/preferencescustomizer.js
+++ defaults/preferences/preferencescustomizer.js
@@ -1,10 +1,11 @@
 pref("extensions.preferencescustomizer.browser.paneMain.enabled", true);
 pref("extensions.preferencescustomizer.browser.paneTabs.enabled", true);
 pref("extensions.preferencescustomizer.browser.paneContent.enabled", true);
-pref("extensions.preferencescustomizer.browser.paneFeeds.enabled", true);
+pref("extensions.preferencescustomizer.browser.paneFeeds.enabled", true); // for old Firefox
 pref("extensions.preferencescustomizer.browser.paneApplications.enabled", true);
 pref("extensions.preferencescustomizer.browser.panePrivacy.enabled", true);
 pref("extensions.preferencescustomizer.browser.paneSecurity.enabled", true);
+pref("extensions.preferencescustomizer.browser.paneSync.enabled", true);
 pref("extensions.preferencescustomizer.browser.paneAdvanced.enabled", true);
 pref("extensions.preferencescustomizer.messenger.paneGeneral.enabled", true);
 pref("extensions.preferencescustomizer.messenger.paneDisplay.enabled", true);
